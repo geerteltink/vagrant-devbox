f0d754dbf00a913c9c99ad88d129836bfeb3ca58
Geert Eltink <xtreamwayz@gmail.com>
2015-01-17 10:10:14 +0100

    Disable phpmyadmin advanced features

--- .puppet/modules/phpmyadmin/manifests/init.pp
+++ .puppet/modules/phpmyadmin/manifests/init.pp
@@ -9,10 +9,18 @@ class phpmyadmin {
     ensure => present,
   }
 
-  file { '/etc/apache2/sites-enabled/001-phpmyadmin.conf':
-    ensure  => link,
-    target  => '/etc/phpmyadmin/apache.conf',
-    require => Package['apache2', 'php5'],
-    notify  => Service['apache2'],
+  file {
+    '/etc/apache2/sites-enabled/001-phpmyadmin.conf':
+      ensure  => link,
+      target  => '/etc/phpmyadmin/apache.conf',
+      require => Package['apache2', 'php5', 'mysql-server', 'phpmyadmin'],
+      notify  => Service['apache2'];
+
+    '/etc/phpmyadmin/config.inc.php':
+      content => template('phpmyadmin/config.inc.php.erb'),
+      owner => 'root',
+      group => 'root',
+      mode  => '0644',
+      require => Package['phpmyadmin'];
   }
 }
