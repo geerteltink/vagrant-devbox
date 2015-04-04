278f7f9e3b775a00571796bb919de12637d54579
Geert Eltink <xtreamwayz@gmail.com>
2015-04-04 21:51:37 +0200

    Add example wordpress setup

---
+++ build/wordpress/public/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require(dirname(__FILE__) . '/wp/wp-blog-header.php');
